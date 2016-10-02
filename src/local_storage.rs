52eb629a13681ee26c7953f113029bd896ac08ce
ticki <ticki@users.noreply.github.com>
2016-10-02 18:27:21 +0200

    Bindings to the localStorage API.

--- src/local_storage.rs
+++ src/local_storage.rs
@@ -1,3 +1,26 @@
-fn get(key: JsString) -> JsString {
+//! Bindings to JavaScript's `localStorage` API.
 
+/// Get the value of some key in the local storage (`localStorage`).
+///
+/// It consumes the key and uses the allocation of the key to store the return value.
+fn get(key: JsString) -> JsString {
+    unsafe {
+        asm!("jsObjects[$0]=localStorage[jsObjects[$0]]"
+             :: "r"(key.get_inner_object().get_id()))
+    }
+
+    key
+}
+
+/// Set the value of some key in the local storage (`localStorage`).
+///
+/// Return the old value.
+fn set(key: JsString, val: JsString) -> JsString {
+    unsafe {
+        asm!("jsObjects[$0]=localStorage[jsObjects[$0]]=jsObjects[$1]"
+             :: "r"(key.get_inner_object().get_id()),
+                "r"(val.get_inner_object().get_id()))
+    }
+
+    key
 }
