e8eacfc6dca52e4aec3baeec1ec11b8b891b4109
ticki <ticki@users.noreply.github.com>
2016-10-02 21:58:31 +0200

    Add bindings to `eval` in the `ffi` module.

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,6 +1,18 @@
 //! JavaScript FFI.
 
+use prelude::*;
+
 /// The integer type of JavaScript.
 type Int = u32;
 
 mod object;
+
+/// Evaluate arbitrary JavaScript code.
+///
+/// # Safety
+///
+/// You can break invariants, and thus it's marked `unsafe`.
+pub unsafe fn eval(js: &JsString) {
+    asm!("eval(__ObjectPool[$0])"
+         :: "r"(js.get_inner_object().get_id()));
+}
